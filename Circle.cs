30a62edeab2ac4e562fa8e7740bea16cbe56e98a
Riya Gupta <griya242k@gmail.com>
2023-02-09 14:44:23 -0500

    containing the completed Circle class

---
+++ Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8170Assign2
+{
+    public class Circle
+    {
+        private int radius;
+
+        public Circle()
+        {
+            radius = 1;
+        }
+        public Circle(int pRadius)
+        {
+            radius = pRadius;
+        }
+        public int GetRadius()
+        {
+            return radius;
+        }
+        public void SetRadius(int pRadius)
+        {
+            if (pRadius>0)
+            {
+                radius = pRadius;
+            }
+            
+        }
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
